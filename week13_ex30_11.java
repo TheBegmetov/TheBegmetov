18075911343ee23317a215c6f3115f8854dc0c9f
TheBegmetov <104788726+TheBegmetov@users.noreply.github.com>
2022-05-03 06:29:21 +0600

    Create week13_ex30_11.java

---
+++ week13_ex30_11.java
@@ -0,0 +1,42 @@
+package com.example.salemjavafx;
+
+public class week13_ex30_11 {
+    String s = "string 1";
+    String s1 = "string 2";
+
+    Thread th = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            System.out.println("Start of thread 1");
+
+            synchronized (s) {
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception ex) {
+                }
+            }
+            System.out.println("End of thread 1");
+        }
+    });
+
+    Thread th1 = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            System.out.println("Start of thread 2");
+            synchronized (s1) {
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception ex) {
+                }
+            }
+            System.out.println("End of thread 2");
+        }
+    });
+
+    public static void main(String[] args) {
+        week13_ex30_11 ex = new week13_ex30_11();
+        ex.th.start();
+        ex.th1.start();
+    }
+}
+
